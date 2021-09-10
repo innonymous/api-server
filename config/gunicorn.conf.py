e6b06e71e06c0721558bc6758b60c7ffde1aaa4d
Ivan Izmailov <f.society.everyday@gmail.com>
2021-09-10 10:30:47 +0300

    add creating logs folder

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -7,5 +7,6 @@ workers = cpu_count()
 worker_class = 'uvicorn.workers.UvicornH11Worker'
 
 _logs_path = Path(environ.get('API_LOGS_PATH', './logs'))
+_logs_path.mkdir(parents=True, exist_ok=True)
 accesslog = str(_logs_path / 'access.log')
 errorlog = str(_logs_path / 'error.log')
