579515868c376cb8f8495cc17c971128b9ef3c26
Ivan Izmailov <f.society.everyday@gmail.com>
2021-09-10 10:16:33 +0300

    add logs path config, add bind config, add example to .env.examplee

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -1,6 +1,11 @@
-from os import cpu_count
+from os import cpu_count, environ
+from pathlib import Path
 
-bind = '0.0.0.0:8000'
+bind = environ.get('API_BIND_URL', '0.0.0.0:8000')
 
 workers = cpu_count()
 worker_class = 'uvicorn.workers.UvicornH11Worker'
+
+_logs_path = Path(environ.get('API_LOGS_PATH', './logs'))
+accesslog = str(_logs_path / 'access.log')
+errorlog = str(_logs_path / 'error.log')
