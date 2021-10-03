831383f54db51e69a4cd46dc7ef8e9e086b97593
Ivan Izmailov <f.society.everyday@gmail.com>
2021-10-03 21:51:33 +0300

    simplified regex

--- innonymous/api/schemas/room/create.py
+++ innonymous/api/schemas/room/create.py
@@ -5,4 +5,4 @@ from pydantic import (
 
 
 class RoomCreateSchema(BaseModel):
-    name: constr(regex=r'^[A-Za-z0-9А-Яа-яЁё][A-Za-z0-9А-Яа-яЁё \-_]{3,30}[A-Za-z0-9А-Яа-яЁё]$')
+    name: constr(regex=r'^[\w0-9][\w0-9\s\-_]{3,30}[\w0-9]$')
